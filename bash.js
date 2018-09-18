07ace44cf9ee5241313a712b41599538113f3d52
Patrick <pdylanconroy@gmail.com>
2018-09-18 11:42:27 -0500

    Patrick Attempt 1

---
+++ bash.js
@@ -0,0 +1,14 @@
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  const pwd = require('./pwd');
+  if (cmd === 'pwd') {
+    pwd();
+  }
+
+  process.stdout.write(`You typed: ` + cmd);
+  process.stdout.write("\nprompt > ");
+
+
+})
---
+++ pwd.js
@@ -0,0 +1,11 @@
+
+ module.exports = function() {
+
+    let pwd = process.cwd();
+
+    process.stdout.write("You current directory: " + pwd);
+    process.stdout.write("\nprompt > ");
+
+
+
+ }
